fa7f5dfb84f1924cd4406a76627a4eb3427f03e1
root <root@nyarlathotep.axfive.net>
2017-02-07 21:46:24 -0700

    update things

--- makekernel/makekernel.sh
+++ makekernel/makekernel.sh
@@ -4,7 +4,6 @@ usage() {
     cat <<USAGE
     $0 [options]
 
-    -c          use ccache
     -h          this help menu
     -r          reboot
 USAGE
@@ -14,9 +13,6 @@ after() { :; }
 
 while getopts 'chr' arg; do
     case "$arg" in
-        c)
-            export CCACHE_DIR=/var/tmp/ccache
-            ;;
         h)
             usage
             exit 0
@@ -30,11 +26,5 @@ while getopts 'chr' arg; do
             ;;
     esac
 done
-
-if [ -n "$CCACHE_DIR" ]; then
-    export PATH="/usr/lib64/ccache/bin:$PATH"
-    export CC='ccache gcc'
-    export CXX='ccache g++'
-fi
 
 make -j10 && make modules_install && make install && genkernel --lvm --luks --busybox initramfs && grub-mkconfig -o /boot/grub/grub.cfg && emerge @module-rebuild && after
